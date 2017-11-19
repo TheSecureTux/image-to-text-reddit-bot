5bef561b85fe26c2c51897bd06061f86762f8780
Tux <jordimirallesguri@gmail.com>
2017-11-19 12:30:46 +0100

    The bot now only returns and downloads the posts with images hosted in reddit.

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 import praw
-# import pyocr
-
+import wget
 
 reddit = praw.Reddit('bot1')
 
 subreddit = reddit.subreddit("ProgrammerHumor")
 
 for submission in subreddit.hot(limit=10):
-    if reddit.domain('i.redd.it'):
-        print("Title: ", submission.title)
-        print("Text: ", submission.selftext)
-        print("Score: ", submission.score)
-        print("---------------------------------\n")
-        print("Subdomain", submission.domain)
+    submissiondomain = submission.domain
+    if submissiondomain == 'i.redd.it':
+        print("Domain: ", submissiondomain)
+        print("Title", submission.title)
+        print("URL: ", submission.url)
+        wget.download(submission.url)
+        
