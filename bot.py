5352f2e2af9ea687636d6cd632a0bd97927c3126
jmg87 <jmiralles@tuta.io>
2017-11-20 10:59:16 +0000

    Adding OCR packages (Tesseract)

--- bot.py
+++ bot.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 import praw
-import wget
+import pytesseract
+import requests
+from PIL import Image
+from PIL import ImageFilter
+from StringIO import StringIO
+
+def process_image(url):
+    image = _get_image(url)
+    image.filter(ImageFilter.SHARPEN)
+    return pytesseract.image_to_string(image)
+
+
+def _get_image(url):
+    return Image.open(StringIO(requests.get(url).content))
+
+
+
 
 reddit = praw.Reddit('bot1')
 
@@ -12,5 +28,8 @@ for submission in subreddit.hot(limit=10):
         print("Domain: ", submissiondomain)
         print("Title", submission.title)
         print("URL: ", submission.url)
-        wget.download(submission.url)
-        
+	_get_image(submission.url)
+	process_image(submission.url)       
+
+
+ 
