be8b0b951a32c27bb5de4e5ca9cd4f95fe9b0ae0
jmg87 <jmiralles@tuta.io>
2017-11-20 11:14:12 +0000

    building pytesseract to process the images

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import wget
 import praw
 import pytesseract
 import requests
@@ -28,8 +29,9 @@ for submission in subreddit.hot(limit=10):
         print("Domain: ", submissiondomain)
         print("Title", submission.title)
         print("URL: ", submission.url)
-	_get_image(submission.url)
+	# _get_image(submission.url)
 	process_image(submission.url)       
+	print(pytesseract.image_to_string(Image.open(_get_image(submission.url))))
 
 
  
