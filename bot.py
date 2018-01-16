076c582f544eea73140473adde80a34741d4a980
jmg87 <jmiralles@tuta.io>
2018-01-16 10:45:28 +0100

    Code tidy up, rewrite, add text as code block

--- bot.py
+++ bot.py
@@ -1,63 +1,70 @@
 #!/usr/bin/python
 import os
-import re
 import sys
 import praw
 from StringIO import StringIO
 from google.cloud import vision
 from google.cloud.vision import types
 
+# Define PRAW parameters
 
+reddit = praw.Reddit('bot1') # bot1 defined in file praw.ini in located in the same folder
 
-# Function to post the text (ocr_data) to reddit as a comment. Autotext added.
+# Ask on which subreddit we are going to work:
+
+subredditToScan = raw_input("Which subreddit would you like to scan?: ")
+subreddit = reddit.subreddit(subredditToScan)
+
+#Function to call the Cloud Vision API and fetch it the target urls
+
+def scan_images(url):
+    client  = vision.ImageAnnotatorClient()
+    image = types.Image()
+    image.source.image_uri = url
+    response = client.text_detection(image=image)
+    text = response.text_annotations
+    return text[0].description
+
+#Function to format the text to post it as a code block on reddit
+
+def format_text(rawtext):
+    formatted_text = ""
+    for line in rawtext.split("\n"):
+        formatted_text += "    " + line + "\n  "
+    return formatted_text
+
+#Function to post the formatted text to reddit
+
 def post_comment(ocr_data):
     submission.reply(ocr_data + "\n\n __________________________________________ \n\n This is a bot still in beta, but the OCR has been improved (hopefully). Please direct all hate and complaints to my master /u/audscias , thank you, puny humans\n ^^r/image_to_text_beta")
 
 
-# Call to OCR API
-def detect_text_uri(uri):
-    """Detects text in the file located in Google Cloud Storage or on the Web.
-    """
-    client = vision.ImageAnnotatorClient()
-    image = types.Image()
-    image.source.image_uri = uri
-
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-
-    for text in texts:
-        textdescription = ("    "+ text.description )
-        return textdescription
-
-# Declare the bot instance it's going to be pick the details for on praw.ini
-reddit = praw.Reddit('bot1')
-
-# Declare in which subreddit are we going to work
-subredditToScan = raw_input("Which subreddit would you like to scan?: ")
-subreddit = reddit.subreddit(subredditToScan)
 
 
 
-# Loop through the subreddit submissions and actually do the magic
+
+#Find the top 10 hot submissions on the subreddit and check wether we already scanned them or not
+
 for submission in subreddit.hot(limit=10):
-    # Let's check if have a list of posts we already commented
-    if not os.path.isfile("posts_replied_to.txt"):
-        posts_replied_to = []
+    if not os.path.isfile("posts_replied_to.txt"): #Create the replied to list file if it doesn't exist
+        with open("posts_replied_to.txt","a"):
+            if submission.domain == "i.redd.it" :
+                text = scan_images(submission.url)
+                formatted_text = format_text(text)
+                post_comment(formatted_text)
+                with open("posts_replied_to.txt","w") as f:
+                    for post_id in posts_replied_to:
+                        f.write(post_id + "\n")
+
     else:
-        # Open the list and read one by one. Also get the domain where it's posted
-        with open("posts_replied_to.txt", "r") as f:
-            submissiondomain = submission.domain
+        with open("posts_replied_to.txt","r") as f:
             posts_replied_to = f.read()
             posts_replied_to = posts_replied_to.split("\n")
-            posts_replied_to = list(filter(None, posts_replied_to))
-            # Check if domain is i.redd.it and not already commented on:
-            if submissiondomain == 'i.redd.it' and submission.id not in posts_replied_to:
-                post_content =  detect_text_uri(submission.url)
-                print post_content
-                # Post comment on reddit
-                post_comment(post_content)
-                # Add post id to the list
-                posts_replied_to.append(submission.id)
-                with open("posts_replied_to.txt", "w") as f:
+            posts_replied_to = list(filter(None,posts_replied_to))
+            if submission.domain == "i.redd.it" and submission.id not in posts_replied_to:
+                text = scan_images(submission.url)
+                formatted_text = format_text(text)
+                post_comment(formatted_text)
+                with open("posts_replied_to.txt","w") as f:
                     for post_id in posts_replied_to:
                         f.write(post_id + "\n")
